0a020f83dc2621040c6c8f9222d4ab728d5cc307
victorsouza2382 <victorsouza2382@gmail.com>
2020-05-15 00:58:01 -0300

    Login em produção

---
+++ Workspace/Farmacia/src/br/com/farmacia/domain/Usuario.java
@@ -0,0 +1,33 @@
+package br.com.farmacia.domain;
+
+import java.io.Serializable;
+import br.com.farmacia.domain.Usuario;
+
+
+public class Usuario implements Serializable {
+	
+	private Long id;
+	private String nome;
+	private String senha;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
+
